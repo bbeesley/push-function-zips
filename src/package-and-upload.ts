55cb3e8965456993ba386ba2f7f6738324cc8e72
Bill Beesley <bill.beesley@soundcloud.com>
2023-01-11 15:57:54 +0000

    docs: update readme

--- src/package-and-upload.ts
+++ src/package-and-upload.ts
@@ -4,6 +4,13 @@ import { packageAndUploadLambda } from './lambda.js';
 
 export * from './@types/index.js';
 
+/**
+ * Packages up an asset based on globs and uploads zips to storage
+ *
+ * @exports
+ * @param {Options} arg Configuration defining what to upload to where
+ * @returns {*}  {Promise<void>}
+ */
 export async function packageAndUpload(arg: Options): Promise<void> {
   if (arg.platform === Platform.AWS) await packageAndUploadLambda(arg);
   if (arg.platform === Platform.GCP) await packageAndUploadCloudFn(arg);
